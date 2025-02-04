4f9b243521579f6710388680ffac538a8eebf63e
Guilherme50 <guilhermex12g@gmail.com>
2025-02-04 08:04:21 -0300

    Create L6-a.cs

---
+++ L6-a.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace name
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			DateTime data = DateTime.Now;
+			
+			Console.WriteLine("A hora atual é: " +data);
+			
+			
+			   Console.WriteLine("Presione algo para sair . . . ");
+			   Console.ReadKey(true);
+			}
+		}
+	}
