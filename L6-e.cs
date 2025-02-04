5f8666eaa32da5542099a07084bedd16e78ea968
Guilherme50 <guilhermex12g@gmail.com>
2025-02-04 10:25:51 -0300

    Create L6-e.cs

---
+++ L6-e.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace name
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("====Escolha uma opçao====");
+			Console.WriteLine("1 - Opçao 1");
+			Console.WriteLine("2 - Opçao 2");
+			Console.WriteLine("3 - Opçao 3");
+			Console.WriteLine("=========================");
+			
+			string opcaoS = Console.ReadLine();
+			int opcao;
+			
+			if (int.TryParse(opcaoS, out opcao )) {
+				switch (opcao) {
+					case 1 :
+						Console.WriteLine("Bem vindo ao programa!. :)");
+						break;
+					case 2 : 
+						Console.WriteLine("Ai ze da manga");
+						break;
+					case 3 :
+						Console.WriteLine("Adeus!");
+						break;
+					default: 
+						Console.WriteLine("Opçao inválida :( . Por Favor escole uma opçao entre 1 2 é 3");
+						break;
+				}					
+			}else {
+				Console.WriteLine("Porfavor insira um número.");
+			
+			}
+			   Console.WriteLine("Presione algo para sair . . . ");
+			   Console.ReadKey(true);
+			}
+		}
+	}
